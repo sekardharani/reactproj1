0c898062c7e6bb1a669526b73a5bcb2c91fd4ddf
sekardharani <dsinz.technology@gmail.com>
2019-07-05 02:37:22 +0530

    First component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 import "./App.scss";
-import Navbar from "./components/Navbar/Navbar";
-import TourList from "./components/TourList";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Navbar />
-        <TourList />
+        <h1>Hello App</h1>
       </React.Fragment>
     );
   }
