b1e3fd85159c020ac748c2a5d3daa6f33cbf80b5
sekardharani <dsinz.technology@gmail.com>
2019-07-25 11:36:10 +0530

    added router bootstrap styled icons packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.scss";
 class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <h1>Hello App</h1>
+      <h1>Hello App 1</h1>
       </React.Fragment>
     );
   }
